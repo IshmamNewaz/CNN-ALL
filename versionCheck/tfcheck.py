0c230f538e65a6803ee08462339f1e09d306079f
Ishmam Newaz <ishu.newaz@gmail.com>
2024-06-19 14:58:26 +0600

    Added CNN by Keras Implementation

---
+++ versionCheck/tfcheck.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+print(tf.__version__)
+
+from tensorflow.python.client import device_lib
+
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+print(get_available_devices())
